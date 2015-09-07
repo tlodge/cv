76b4c55c8793c65e8656e3f5147867618e5bf58d
tlodge <tlodge@gmail.com>
2015-09-07 11:12:23 -0400

    cp commit

--- _site/js/my/cinc.js
+++ _site/js/my/cinc.js
@@ -1,6 +1,23 @@
 define(['jquery','knockout', 'd3building', 'knockoutpb'], function($, ko, d3building){
 	
-	d3building.init();
+	d3building.init({
+		width: function (){
+			return $(document).width();
+		},
+		
+		height: function(){
+			return $(document).height();
+		},
+		
+		x: function(){
+			return 0;
+		},
+		
+		y: function(){
+			return 0;
+		},
+		
+	});
 	
 	var 
 		id = "cinc",
