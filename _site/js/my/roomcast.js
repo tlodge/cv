1ed86608c28394f056e8f674b22551a31f279e6b
tlodge <tlodge@gmail.com>
2015-09-07 08:45:05 -0400

    cp commit

--- _site/js/my/roomcast.js
+++ _site/js/my/roomcast.js
@@ -5,13 +5,22 @@ define(['jquery','knockout', 'knockoutpb'], function($, ko){
 		
         selectedItem    = ko.observable().subscribeTo("menu_event", true),
 
+		menuItems = ko.observableArray([
+			{id: 'panel', name: 'panel'},
+			{id: 'login',  	name:'login'},
+			{id: 'maker', name: 'maker'},
+			{id: 'dashboard',  	name:'dashboard'},
+			{id: 'backend', name:'backend'},
+		]),
+		
         amSelected      = ko.computed(function(){
             console.log("checking selected for id " + id);
             return id === selectedItem().id
         });
         
     return {
-		amSelected: amSelected
+		amSelected: amSelected,
+		menuItems: menuItems,
     }
 
 });
