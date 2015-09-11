764ae8f63fb638f0979812758e0a0c77b601200d
tlodge <tlodge@gmail.com>
2015-09-11 09:05:51 -0400

    cp commit

--- js/my/erith.js
+++ js/my/erith.js
@@ -14,13 +14,13 @@ define(['jquery','knockout', 'knockoutpb'], function($, ko){
 			},
 			{
 				id: 'tagging',  	
-				name:'panel', 
-				img: '../assets/img/erithtagging.jpg', 
+				name:'tagging', 
+				img: '../assets/img/erithtag.jpg', 
 				html: "<h4>erith park</h4> <p class='text-justify'>	<p> This situated display is being installed in Erith Park, a development in South East London.  The code is written in <strong>react/flux and D3</strong> and hooks up with a <strong>nodejs</strong> backend, <strong>socket.io</strong> for live updates and simple <strong>MySQL</strong> database. There is a also a simple android container which is used to run it full screen on android tablets </p>"
 			},	
 			{
 				id: 'admin',  	
-				name:'register', 
+				name:'admin', 
 				img: '../assets/img/erithadmin.jpg', 
 				html: "<h4>erith park</h4> <p class='text-justify'>	<p> All images are seen immediately on an admin iterface where they can be moderated.  The admin interface allows managers to send messages to the main display and to update the selection of tags that can be chosen for an image </p>"
 			},	
